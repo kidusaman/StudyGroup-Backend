14e84f4638ecf4f73f56d2467f4c4bd819ab8105
Kidus <165596324+kidusaman@users.noreply.github.com>
2025-03-02 10:31:01 -0600

    db.js

--- db.js
+++ db.js
@@ -1,10 +1,11 @@
 // db.js
 import pkg from 'pg';
 const { Pool } = pkg;
+import dotenv from 'dotenv';
+dotenv.config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
+  connectionString: process.env.DB_URL,
 });
 
 export default pool;
